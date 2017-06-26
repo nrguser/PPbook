7280539a6b9b7cfccaf5b47d450c28159c1b6f7d
Matthew Buck <nrguser@gmail.com>
2017-06-25 22:23:42 -0500

    Got the header file found and did the next example, will be commiting more than what is usually required, but want to make sure that I can get this figured out and the commands don't fade from memory

--- Chapter 2/Chapter2/Chapter2/main.cpp
+++ Chapter 2/Chapter2/Chapter2/main.cpp
@@ -1,4 +1,8 @@
+/*
+
+
 // The "std_lib_facilities.h" will not work as this is an empty project, will have to look into it having the correct header file.
+// Found the header file on the website, good to go now!
 
 
 #include <iostream>
@@ -8,5 +12,19 @@ using namespace std;
 int main()
 {
 	cout << "Hello, World! \n";
+	return 0;
+}
+
+// \n indicates a new line, can be used instead of endl;
+
+
+*/
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Hello, World \n";
+	keep_window_open();
 	return 0;
 }
