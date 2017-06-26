bb1342088c0fb9c825e8e2c5bb781d6de30362f2
Matthew Buck <nrguser@users.noreply.github.com>
2017-06-25 19:08:39 -0500

    Update main.cpp
    added another line to pest pull

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -6,5 +6,6 @@ int main()
 {
 	cout << "Hello World" << endl;
 	cout << " I added this line as well, just to show I can commit this." << endl;
+	cout << "Now let's see if I can pull this as well." << endl;
 	return 0;
 }
