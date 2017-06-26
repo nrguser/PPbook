11aa55022dbfc9c4a8557abcf5dafc0b8c94c3f9
Matthew Buck <nrguser@gmail.com>
2017-06-25 22:07:24 -0500

    Started chaper 2, had to modify the header as the header file does not exist on an empty project

---
+++ Chapter 2/Chapter2/Chapter2/main.cpp
@@ -0,0 +1,12 @@
+// The "std_lib_facilities.h" will not work as this is an empty project, will have to look into it having the correct header file.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World! \n";
+	return 0;
+}
